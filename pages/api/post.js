d672571bbca755481b89302f075f9be3529bd6aa
AetheryNA <mrnaaw98@gmail.com>
2021-08-09 17:33:07 +0530

    Post api with image upload to local server :smile:

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,20 +1,40 @@
 import nc from 'next-connect'
 import withSession from '../../lib/auth/session'
 import Post from '../../lib/models/post'
+import multer from 'multer'
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req, file, cb) => cb(null, file.originalname),
+  })
+})
+
+const fileUpload = upload.single('file')
 
 const handler = nc()
+  .use(fileUpload)
   .use(withSession)
   .post(async(req, res) => {
+    const data = req.body
+
+    const postingData = {
+      image_url: req.file.path,
+      content: data.content,
+      like_count: parseInt(data.like_count),
+    }
+
     const user = req.session.get('user')
     const userId = {
       user_id : user.id
     }
-    const data = JSON.parse(req.body)
-    const postData = Object.assign(userId, data)
+    
+    const postData = Object.assign(userId, postingData)
 
     const newPost = new Post(postData)
 
     await newPost.createPost().catch((err) => {
+      console.log(err);
       return res
         .status(400)
         .json({
@@ -29,3 +49,9 @@ const handler = nc()
   })
 
 export default handler
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
